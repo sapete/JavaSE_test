d97327a83d32b250d3525fe2dcd29994e2ffa9ba
sapete <hanxubuaa1107@126.COM>
2013-08-03 15:44:52 +0800

    Initial commit

---
+++ Day5/src/helloworld.java
@@ -0,0 +1,20 @@
+//import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+/**
+ * Created by Administrator on 13-8-3.
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        float v=12;
+        double v1=21.3232;
+        String s="hg";
+        System.out.println(v1+s);
+        boolean a=true;
+        boolean b=false;
+
+        System.out.println(a|b);
+        System.out.println(b==a);
+
+    }
+}
